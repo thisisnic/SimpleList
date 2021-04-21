6130983dbb6bb752e9be87a708e10a6cc034cd2f
Nic Crane <thisisnic@gmail.com>
2021-04-21 10:15:19 +0100

    Add more functionality

--- main.cpp
+++ main.cpp
@@ -36,9 +36,9 @@ int main(int arg_count, char *args[]){
 
 void print_menu(string name){
     int choice;
-
+    cout << "\n\n\n\n\n\n\n\n";
     cout << "***********************\n";
-    cout << " 1 = Print list.\n";
+    cout << " 1 - Print list.\n";
     cout << " 2 - Add to list.\n";
     cout << " 3 - Delete from list.\n";
     cout << " 4 - Quit.\n";
@@ -48,8 +48,66 @@ void print_menu(string name){
 
     if(choice == 4){
         exit(0);
+    } else if (choice == 2){
+        add_item();
+    } else if (choice == 1){
+        print_list();
+    } else if(choice == 3){
+        delete_item();
     } else {
         cout << "Sorry, choice not implemented yet\n";
+    }
+
+}
+
+void add_item(){
+    cout << "\n\n\n\n\n\n\n\n";
+    cout << "*** Add item ***\n";
+    cout << "Type in an item and press enter: ";
+    string item;
+    // capture the output from the console and put it into variable 'item'
+    cin >> item;
+
+    // push back an item into our vector list (i.e. to the bottom of our vector, )
+    list.push_back(item);
+
+    cout << "Successfully added an item to the list! \n\n\n\n\n";
+    cin.clear();
+    print_menu(name);
+}
+
+void delete_item(){
+    cout << "\n\n\n\n\n\n\n\n";
+    cout << "*** Delete item ***\n";
+    cout << "Select an item index number to delete\n";
+    if (list.size()){
+        for(int i = 0; i < list.size(); i++){
+            cout << i <<  ": " << list[i] << "\n";
+        }
+    } else {
+        cout << "No items to delete. \n";
+    }
+
+    print_menu(name);
+
+}
+
+void print_list(){
+    cout << "\n\n\n\n\n\n\n\n";
+    cout << "*** Printing list ***\n";
+
+    for(int list_index = 0; list_index < list.size(); list_index++){
+        cout << " * " << list[list_index] << "\n";
+    }
+
+    cout << "M - Menu \n";
+    char choice;
+    cin >> choice;
+
+    if(choice == 'M' || choice == 'm'){
+        print_menu(name);
+    } else {
+        cout << "Invalid Choice. Quitting..\n";
     }
 
 }
